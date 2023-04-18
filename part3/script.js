446d83bd148da2aebcc905a2531b45b5f32bf416
triplexlab <thdbsgh3443@naver.com>
2023-04-18 17:13:14 +0900

    Add files via upload

--- part3/script.js
+++ part3/script.js
@@ -1,16 +1,17 @@
 let diary = "";
-let emoji = "";
-
 function saveHandler() {
-  const dateString = $("#date").val().replace(/-/g, "/"); // date YYYY/MM/DD 형태로 변경
+  const date = new Date($("#date").val());
   const text = $("#text").val();
-
-  if(dateString === "" || text === "")return false;
-
+  const mood = $("#mood").val();
+  const dateString = date.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
   diary = `
     <a href="#0" class="list-group-item list-group-item-action">
       <div class="d-flex w-100 justify-content-between">
-        <small>${emoji}</small>
+        <small>${mood}</small>
         <small>${dateString}</small>
       </div>
       <h5 class="mb-1">${text}</h5>
@@ -21,22 +22,12 @@ function saveHandler() {
 
   $("#date").val('');
   $("#text").val('');
+  $("#mood").val('');
 }
-function emojiSelected(_, value) {
-  const emojiFit = this.emojis.filter((ele, idx) => {
-    if(idx+1 === Number(value)){
-      return ele;
-    }
-  });
-  emoji = emojiFit;
-};
 
 $(document).ready(function() {
   diary = localStorage.getItem("diary");
   if(diary === null) diary = "";
-  $("#emoji").emoji({
-    callback: emojiSelected
-  });
   $("#result").html(diary);
   $('#save').click(saveHandler)
-}); 
+});
